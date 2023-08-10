13e290416fedb43c54713b845f1bb99a2792b440
duxpixels <duxwork0@gmail.com>
2023-08-10 14:35:47 +0300

    Home (RightSidebar)

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client";
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <section className="rightsidebar custom-scrollbar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Users
+        </h3>
+      </div>
+    </section>
   )
 }
 
