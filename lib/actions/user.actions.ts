7d8a1af9897ee067c22dd09a3de6b2ede6d0de8d
duxpixels <duxwork0@gmail.com>
2023-08-18 16:35:23 +0300

    Search, UserCard

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Community from "../models/community.model";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Props {
   id: string;
@@ -63,23 +64,70 @@ export const fetchUserPosts = async (userId: string) => {
   try {
     connectToDB();
 
-    const threads = await User.findById(userId)
-    .populate({
-      path:"threads",
-      model:Thread,
-      populate:{
-        path:"children",
-        model:Thread,
-        populate:{
-          path:"author",
-          model:User,
-          select:"image name id"
-        }
-      }
-    })
+    const threads = await User.findById(userId).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "image name id",
+        },
+      },
+    });
 
-    return threads
-  } catch (err:any) {
+    return threads;
+  } catch (err: any) {
     throw new Error(`Failed to fetch user posts: ${err.message}`);
+  }
+};
+
+export const fetchUsers = async ({
+  userId,
+  searchString="",
+  pageNumber=1,
+  pageSize=20,
+  sortBy="desc"
+}:{
+  userId:string,
+  pageNumber?:number,
+  pageSize?:number,
+  searchString?:string,
+  sortBy?:SortOrder,
+}) => {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber-1)* pageSize;
+
+    const regex = new RegExp(searchString,"i")
+
+    const query:FilterQuery<typeof User> = {
+      id:{$ne:userId}
+    }
+
+    if(searchString.trim() !== ""){
+      query.$or = [
+        {username:{$regex:regex}},
+        {name:{$regex:regex}}
+      ]
+    }
+
+    const sortOptions = {joinedAt:sortBy}
+
+    const usersQuery = User.find(query).sort(sortOptions).skip(skipAmount).limit(pageSize)
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + users.length;
+
+    return {users,isNext}
+
+  } catch (err:any) {
+    throw new Error(`Failed to search users: ${err.message}`);
   }
 };
